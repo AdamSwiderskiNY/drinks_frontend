2fc4aff5d7643511cbf68a9657dad39efe7705b1
Adam-Swiderski <adam.swiderski@noseryoung.com>
2024-09-05 14:23:51 +0200

    forgot to add the unversioned files, now pushed

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import './App.css'
+import Homepage from "./pages/Homepage.tsx"
+import DrinksPage from "./pages/DrinksPage.tsx";
+import NotFoundPage from "./pages/NotFoundPage.tsx";
+import Loginpage from "./pages/Loginpage.tsx";
+
+function App() {
+
+    return (
+        <>
+            <div>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Loginpage />} />
+                        <Route path='/home' element={<Homepage />}/>
+                        <Route path='/drinks' element={<DrinksPage />}/>
+                        <Route path='/*' element={<NotFoundPage />}/>
+
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </>
+    )
+}
+
+export default App
